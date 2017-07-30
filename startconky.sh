69a3c240e5d3847141e91faf00a5d74b66f04ca9
Tsubasa Toki <erwincat0812@gmail.com>
2017-07-30 10:19:27 +0900

    edit startup time

--- startconky.sh
+++ startconky.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+sleep 3 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+killall conky
+sleep 1
 conky -c ~/.Conky/rings & # the main conky with rings
-sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
+sleep 3 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
 conky -c ~/.Conky/cpu &
 sleep 1
 conky -c ~/.Conky/mem &
