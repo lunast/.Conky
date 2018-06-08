6e41e97109d880c77960751ccae20b04de3540a5
Tsubasa Toki <erwincat0812@gmail.com>
2018-06-08 11:11:14 +0900

    起動時間するまでの時間を変更

--- startconky.sh
+++ startconky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 3 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+sleep 5 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
 killall conky
 sleep 1
 conky -c ~/.Conky/rings & # the main conky with rings
